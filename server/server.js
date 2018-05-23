57b976f78fab66b00bcf9b69e4bd1e80e3268412
find4u <richard.git@find4u.co.uk>
2018-05-23 11:51:20 +0100

    setup app for heoku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(bodyParser.json());
